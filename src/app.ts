5d17cf2cda5cf4a3ccdb67b57913c3152e3398fb
wookkii <wookkii@home>
2018-11-17 22:46:00 +1100

    Demonstrating .bind - binds context

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,12 @@ myObj.myMethod()
 function myFunction(...text: string[] ) {
     console.log('Function', this, text);
 }
+
+const bindFunction = myFunction.bind(myObj);
+bindFunction('ABC', 'DEF');
+bindFunction('GHI', 'JKL');
+
+
 myFunction('ABC', 'DEF');
 myFunction.call(myObj, 'ABC', 'DEF', 'GHI');
 myFunction.apply(myObj, ['ABC', 'DEF']);
